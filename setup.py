d0f79eeba8d87c31b5646ca0adf147b03651ebec
Dhia Hmila <dhiahmila@gmail.com>
2021-05-24 19:49:59 +0200

    fixed images in README.md

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import re
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -6,9 +7,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read().split('\n')
 
+
+reg = re.compile(r"!\[(?P<alt_text>.*?)\]\((?P<path>.*)\)")
+long_description = re.sub(reg, r"![\g<alt_text>](https://raw.githubusercontent.com/hmiladhia/img2text/master/\g<path>)",
+                          long_description)
+
 setuptools.setup(
     name="img2text",
-    version="0.0.1",
+    version="0.0.2",
     author="Dhia HMILA",
     author_email="dhiahmila@gmail.com",
     description="A lightweight module to generate colorful ascii art from images",
