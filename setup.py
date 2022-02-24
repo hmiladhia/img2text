b493c698cb8e11d4dcc5512c458c9b883a062a6a
Dhia Hmila <dhiahmila@gmail.com>
2022-02-24 17:23:09 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,6 @@ import setuptools
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
-    requirements = fh.read().split('\n')
-
-
 reg = re.compile(r"!\[(?P<alt_text>.*?)\]\((?P<path>.*)\)")
 long_description = re.sub(reg, r"![\g<alt_text>](https://raw.githubusercontent.com/hmiladhia/img2text/master/\g<path>)",
                           long_description)
@@ -23,7 +19,10 @@ setuptools.setup(
     license='MIT',
     url="https://github.com/hmiladhia/img2text",
     py_modules=['img2text'],
-    install_requires=requirements,
+    install_requires=[
+        'Pillow>=8.2.0',
+        'colorama>=0.4.*',
+    ],
     entry_points={
         'console_scripts': ['img2text=img2text:main'],
     },
