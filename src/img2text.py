09a9ec603e5ddaf9fed80bb5f234b277cb79f268
Dhia Hmila <dhiahmila@gmail.com>
2024-02-22 22:02:45 +0100

    refactor colors

--- src/img2text.py
+++ src/img2text.py
@@ -5,12 +5,11 @@ from base64 import decodebytes
 from io import BytesIO
 
 import colorama
-from colorama import Back, Fore, Style
 from PIL import Image
 
 colorama.init()
 
-COLORS_DICT = {
+_COLORS_DICT: dict[str, tuple[int, int, int]] = {
     "BLACK": (0, 0, 0),
     "RED": (255, 0, 0),
     "GREEN": (0, 255, 0),
@@ -21,12 +20,56 @@ COLORS_DICT = {
     "WHITE": (255, 255, 255),
 }
 
-DEFAULT_CHARS = (
+_BACKGROUND_DICT: dict[str, str] = {
+    "BLACK": "\x1b[40m",
+    "BLUE": "\x1b[44m",
+    "CYAN": "\x1b[46m",
+    "GREEN": "\x1b[42m",
+    "LIGHTBLACK_EX": "\x1b[100m",
+    "LIGHTBLUE_EX": "\x1b[104m",
+    "LIGHTCYAN_EX": "\x1b[106m",
+    "LIGHTGREEN_EX": "\x1b[102m",
+    "LIGHTMAGENTA_EX": "\x1b[105m",
+    "LIGHTRED_EX": "\x1b[101m",
+    "LIGHTWHITE_EX": "\x1b[107m",
+    "LIGHTYELLOW_EX": "\x1b[103m",
+    "MAGENTA": "\x1b[45m",
+    "RED": "\x1b[41m",
+    "RESET": "\x1b[49m",
+    "WHITE": "\x1b[47m",
+    "YELLOW": "\x1b[43m",
+}
+_FOREGROUND_DICT: dict[str, str] = {
+    "BLACK": "\x1b[30m",
+    "BLUE": "\x1b[34m",
+    "CYAN": "\x1b[36m",
+    "GREEN": "\x1b[32m",
+    "LIGHTBLACK_EX": "\x1b[90m",
+    "LIGHTBLUE_EX": "\x1b[94m",
+    "LIGHTCYAN_EX": "\x1b[96m",
+    "LIGHTGREEN_EX": "\x1b[92m",
+    "LIGHTMAGENTA_EX": "\x1b[95m",
+    "LIGHTRED_EX": "\x1b[91m",
+    "LIGHTWHITE_EX": "\x1b[97m",
+    "LIGHTYELLOW_EX": "\x1b[93m",
+    "MAGENTA": "\x1b[35m",
+    "RED": "\x1b[31m",
+    "RESET": "\x1b[39m",
+    "WHITE": "\x1b[37m",
+    "YELLOW": "\x1b[33m",
+}
+_BACKGROUND_RESET: str = "\x1b[49m"
+_FOREGROUND_RESET: str = "\x1b[39m"
+_BRIGHT_STYLE: str = "\x1b[1m"
+_STYLE_RESET_ALL: str = "\x1b[0m"
+
+_DEFAULT_CHARS = (
     r" .'`^\"\,:;Il!i><~+_-?][}{1)(|\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 )
 
 __author__ = "Dhia Hmila"
 __version__ = "0.1.1"
+__all__ = ["img_to_ascii", "to_ascii", "__version__", "__author__"]
 
 
 def to_ascii(
@@ -44,23 +87,23 @@ def to_ascii(
     img = img.convert("RGB" if colorful else "L")
 
     if bg_color:
-        bg_color = vars(Back)[bg_color.upper()]
+        bg_color = _BACKGROUND_DICT[bg_color.upper()]
 
-    chars = chars or DEFAULT_CHARS
+    chars = chars or _DEFAULT_CHARS
     if reverse:
         chars = chars[::-1]
 
     pixels = img.load()
-    text_img = [Style.BRIGHT] if bright else []
+    text_img = [_BRIGHT_STYLE] if bright else []
     w, h = img.size
     for i in range(h):
         line = bg_color if bg_color else ""
         for j in range(w):
             line += _get_pixel(pixels[j, i], chars, bg_color)
-        line += Back.RESET if bg_color else ""
+        line += _BACKGROUND_RESET if bg_color else ""
         text_img.append(line)
     if bright:
-        text_img.append(Style.RESET_ALL)
+        text_img.append(_STYLE_RESET_ALL)
     return "\n".join(text_img)
 
 
@@ -209,7 +252,7 @@ def _resize(
 
 def _pick_color(r: int, g: int, b: int, bg_color: str | None = None) -> str:
     m, s = "BLACK", 255**2 * 3
-    for color, (cr, cg, cb) in COLORS_DICT.items():
+    for color, (cr, cg, cb) in _COLORS_DICT.items():
         score = (cr - r) ** 2 + (cg - g) ** 2 + (cb - b) ** 2
         if color != bg_color and score < s:
             m = color
@@ -227,9 +270,9 @@ def _get_pixel(
 
     r, g, b = pixel
     color = _pick_color(r, g, b, bg_color)
-    color = vars(Fore)[color]
+    color = _FOREGROUND_DICT[color]
     grayscale = 0.2989 * r + 0.5870 * g + 0.1140 * b
-    return color + chars[int(grayscale * len(chars) / 256)] + Fore.RESET
+    return color + chars[int(grayscale * len(chars) / 256)] + _FOREGROUND_RESET
 
 
 if __name__ == "__main__":
